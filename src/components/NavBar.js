c6a9bac2ed21122c0596e8ac0522914b99bc7a40
woodelin-florveus <woodelinflorveus@gmail.com>
2021-02-21 00:40:42 -0500

    up to proItems

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,19 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  // return <nav>{/* display an <a> tag for each link here */}</nav>;
+  // return <nav><a href="#Home">Home</a></nav>;
+
+  const navElements = links.map((link) => {
+    // console.log(link)
+    return <a href={link}>{link}</a> 
+  });
+    return   <nav> {navElements} </nav>
+    
+        
+    
+  
+
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,13 +2,36 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  // console.log(projects);
+
+  const projectElement = projects.map((proObj) => {
+
+    return (
+      <ProjectItem 
+    key = {proObj.key}
+    name = {proObj.name}
+    about = {proObj.about}
+    />
+    )
+
+  })
+
+  // console.log(projectElement)
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projectElement}
+      </div>
     </div>
   );
+
+
+    
+    
+
+
 }
 
 export default ProjectList;
